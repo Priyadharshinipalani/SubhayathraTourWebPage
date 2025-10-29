2bb25f446c9bd2ebc8b97f13d74eaaa384dfd626
Priyadharshini Palani <85474421+Priyadharshinipalani@users.noreply.github.com>
2025-10-29 12:02:12 +0530

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,9 @@
 import Hero from '../components/Hero';
-import Services from '../components/Services';
-import Packages from '../components/Packages';
-import Testimonials from '../components/Testimonials';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <Services />
-      <Packages />
-      <Testimonials />
     </>
   );
 };
